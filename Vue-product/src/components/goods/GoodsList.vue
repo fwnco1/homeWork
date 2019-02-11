6f60b7c3111bb03aa6feaa95e9c5a463e813bab0
fwcno1 <190404614@qq.com>
2019-02-11 13:29:28 +0800

    chuqi

---
+++ Vue-product/src/components/goods/GoodsList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="goods-list">
+        <h3>这是商品列表页面</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
